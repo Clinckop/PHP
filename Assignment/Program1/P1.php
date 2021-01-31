c4dc16b8bb1d763e463438e509c5fa23bc54218e
Harit Patel <75656894+Clinckop@users.noreply.github.com>
2021-01-31 22:31:29 +0530

    Create P1.php

---
+++ Assignment/Program1/P1.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <?php
+            function temp($department)
+            {
+                echo "<h1>$department <br></h1>";
+            }
+        ?>
+    </body>
+</html>
+
+<?php
+    temp('Indus University');
+?> 
